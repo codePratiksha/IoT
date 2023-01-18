e1109c5d33e9a07d98a184f225af8f0ce424a713
codePratiksha <harshepratiksha0@gmail.com>
2023-01-18 09:41:39 +0530

    added substraction feature for two numbers

--- calculator.py
+++ calculator.py
@@ -1,4 +1,12 @@
 def add(num1,num2):
         return num1+num2
+
+def sub(num1,num2):
+        return num1-num2
+        
 res_add = add(10,20)
 print(res_add)
+
+res_sub = sub(10,20)
+print(res_sub)
+
