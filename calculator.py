b726463ea184ca559c43483a66cd60bfdee97ba5
codePratiksha <harshepratiksha0@gmail.com>
2023-01-18 10:32:27 +0530

    div of 2

--- calculator.py
+++ calculator.py
@@ -6,6 +6,9 @@ def sub(num1,num2):
 def mul(num1,num2):
         return num1*num2
 
+def div(num1,num2):
+        return num1/num2
+
 
 res_add = add(10,20)
 print(res_add)
@@ -15,5 +18,9 @@ print(res_sub)
 
 res_mul = mul(10,20)
 print(res_mul)
+
+res_div = div(10,20)
+print(res_div)
+
 
 
