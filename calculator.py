170b390ffc94b7a5ff1453ed37acbdd1489a06d5
codePratiksha <harshepratiksha0@gmail.com>
2023-01-18 10:26:17 +0530

    mul of two

--- calculator.py
+++ calculator.py
@@ -3,10 +3,17 @@ def add(num1,num2):
 
 def sub(num1,num2):
         return num1-num2
-        
+def mul(num1,num2):
+        return num1*num2
+
+
 res_add = add(10,20)
 print(res_add)
 
 res_sub = sub(10,20)
 print(res_sub)
+
+res_mul = mul(10,20)
+print(res_mul)
+
 
